a9690956d783b3fac771096dd021765ad551f37c
omkar213 <omkardhumal213@gmail.com>
2022-02-24 12:44:45 +0530

    fourth commit

--- src/UserDetail.js
+++ src/UserDetail.js
@@ -3,15 +3,15 @@ import './App.css';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 
 
-const UserDetail = () => {
-  const { id} = useParams();
+const UserDetail = (props) => {
+  const {id} = useParams();
   console.log(id);
   return (
     <>
       <div>
           <h1>UserImage</h1>
           <strong>User ID: {id}<br /></strong>
-          {/* Image: <img className='usersImg' src={id.url} alt="Image" /> */}
+          Image: <img className='usersImg' src={id.url} alt="Image" />
       </div>
     </>
   )
